370896150e515b0f5867ff41038dd18c0abcd858
Clon54 <clon5454@outlook.com>
2023-01-27 16:11:41 -0500

    logre que funcionaria  con while

--- src/Autificador.java
+++ src/Autificador.java
@@ -23,5 +23,27 @@ public class Autificador {
                 System.out.println("tiene otro intento, este es su intento numero "+i);
             }
         }
+        //ahora con while 
+
+        int a = 1;
+        boolean master = true;
+        System.out.println("por favor coloque otra vez la contraseña");
+
+        while(master){
+            String Password = input.nextLine();
+
+            if(Password.equals(Contrasena)){
+                // eguals es metodo si ve si otro objeto es igual al otro
+                System.out.println("Enhorabuena");
+                break;
+            }if (a == 3) {
+                System.out.println("usted no se sabe su contraseña");   
+            } 
+            else{
+                System.out.println("tiene otro intento, este es su intento numero "+a);
+            }
+            a++;
+        }
+        
     }
 }
